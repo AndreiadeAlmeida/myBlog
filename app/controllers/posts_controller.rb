c702088292e7a6be9b5acdcd6d3a417f3e6da7d8
Andreia <andreia.ca22@gmail.com>
2018-03-21 01:15:59 +0000

    myBlog final

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.limit(4)
+    @posts = Post.limit(@posts)
   end
 
   # GET /posts/1
