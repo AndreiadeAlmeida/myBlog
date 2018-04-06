ad2b49f6c6aebaaff707b6b583727f5e4f398819
Andreia <andreia.ca22@gmail.com>
2018-04-06 21:24:52 +0100

    contact

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,13 @@ class Post < ApplicationRecord
 
   scope :most_recent, -> { order(id: :desc) }
 
+
+  def self.search(search_term)
+    if Rails.env.production?
+      Post.where("name ilike ?", "%#{search_term}%")
+    else
+      Post.where("name LIKE ?", "%#{search_term}%")
+    end
+  end
+  
 end
